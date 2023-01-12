edbb33dc9cb1bd88c5d4ebaa4a8681462cbf0897
colton <adamscolton64@gmail.com>
2023-01-12 11:31:53 -0600

    Day04 Conditionals | ThreeEqual

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,16 @@
 public class ThreeEqual {
     public static void main(String[] args) {
-        
+        try {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+            if (a == b && b == c && c == a ) {
+                System.out.println("The three numbers are equal");
+            } else {
+                System.out.println("the numbers are not equal");
+            }
+        } catch (Exception e) {
+            System.out.println("try again");
+        }
     }
 }
